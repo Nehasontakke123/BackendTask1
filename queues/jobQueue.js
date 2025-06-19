08bf2066d34245dd0b76a1165016634c848ccebd
Nehasontakke123 <nehasontakke1880@gmail.com>
2025-06-19 23:41:38 +0530

    new update

--- queues/jobQueue.js
+++ queues/jobQueue.js
@@ -1,15 +1,30 @@
+// import Bull from 'bull';
+// import dotenv from 'dotenv';
+// dotenv.config();
+
+// const jobQueue = new Bull('job-import-queue', {
+//   redis: {
+//     host: process.env.REDIS_HOST,
+//     port: Number(process.env.REDIS_PORT),
+//     password: process.env.REDIS_PASSWORD,
+//     tls: {}, // ✅ Required for Redis Cloud SSL
+//     maxRetriesPerRequest: null, // ✅ Disable retry limit error
+//   },
+// });
+
+// export default jobQueue;
+
+
+
 import Bull from 'bull';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const jobQueue = new Bull('job-import-queue', {
+const jobQueue = new Bull('job-import-queue', process.env.REDIS_URL, {
   redis: {
-    host: process.env.REDIS_HOST,
-    port: Number(process.env.REDIS_PORT),
-    password: process.env.REDIS_PASSWORD,
-    tls: {}, // ✅ Required for Redis Cloud SSL
-    maxRetriesPerRequest: null, // ✅ Disable retry limit error
-  },
+    tls: {}, // ✅ Needed for SSL connection (Redis Cloud)
+    maxRetriesPerRequest: null
+  }
 });
 
 export default jobQueue;
